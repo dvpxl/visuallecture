2ded9ca80818f7732db0bf63b41d4be3358465b2
--local <--local>
2015-10-24 16:03:20 -0700

    add post request sample

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -11,6 +13,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/sample', function(request, response){
+  response.send(request.body);
 });
 
 app.listen(app.get('port'), function() {
