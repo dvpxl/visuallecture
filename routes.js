49c489011b60cd6deda50970b88ec9bcb2e1e617
jonathan paek <jonathan@dvpxl.com>
2015-10-24 22:49:50 -0700

    front-end stringify response

--- routes.js
+++ routes.js
@@ -11,22 +11,22 @@ module.exports = function(app, io) {
     //scraper.on("image", function(image) {
     //    response.send(image.title);
     //});
-    (scraper.scrape( function(image) {
-        console.log(image);
+	    (scraper.scrape( function(image) {
+	        console.log(image);
 
-    }))();
-    response.send(200);
-  //response.send(request.body);
+	    }))();
+
+    	response.send(200);
+  		//response.send(request.body);
 	});
 
 	app.post('/wiki', wiki);
-
 
 	/***
 	* API to process the speech text
 	***/
 	app.post('/v1/speech/text', function(request, response){
-	  io.emit('speech', 'data');
+	  io.emit('speech', request.body);
 	  response.send('OK.  You Sent: ' + request.body);
 
 	});
