401c528ae562929de404730391d0a64a3bd57300
meenuh <roblox@Setups-MacBook-Pro.local>
2015-10-24 18:39:35 -0700

    Adding image scraper

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var Scraper = require("image-scraper");
+var wiki = require('./wiki-example.js');
+
+var scraper = new Scraper("http://www.wikipedia.org/wiki/potato");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,8 +20,19 @@ app.get('/', function(request, response) {
 });
 
 app.post('/sample', function(request, response){
-  response.send(request.body);
+
+    //scraper.on("image", function(image) {
+    //    response.send(image.title);
+    //});
+    (scraper.scrape( function(image) {
+        console.log(image);
+
+    }))();
+    response.send(200);
+  //response.send(request.body);
 });
+
+app.post('/wiki', wiki);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
